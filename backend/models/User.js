e3ca003d3c108d9d3490c0f49a10a30e538aa4c6
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-31 13:49:20 -0500

    models created

---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Weather = require('./Weather')
+const UserSchema = new mongoose.Schema({
+    name: String,
+    weather: [Weather.Schema]
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
---
+++ backend/models/Weather.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const WeatherSchema = new mongoose.Schema({
+    location: String,
+    current_temp: String,
+    feels_like: String,
+    condition: String
+})
+
+const Weather = mongoose.model('Weather', WeatherSchema)
+module.exports = User
