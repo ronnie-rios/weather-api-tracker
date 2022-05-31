4a912ff9ee682f28a2335a89a85ea7c94bc3e730
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-31 13:53:44 -0500

    server setup

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 5001
+
+app.use(require('cors')({ origin: /localhost/ }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/weatherdb')
+
+const userController = require('./routes/userController')
+app.use('/user', userController)
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
